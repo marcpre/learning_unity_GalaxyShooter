68b7e7b4db5c0e21c2e51277a52dd27b5d5a076f
marcpre <marcus.presich@gmail.com>
2018-11-18 08:22:50 +0100

    code cleanup

--- Assets/Galaxy Shooter/Scripts/Player.cs
+++ Assets/Galaxy Shooter/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public float speed = 5.0f;
+    [SerializeField]
+    private float speed = 5.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -16,15 +17,22 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+        Movement();
+    }
+
+
+    private void Movement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime); // "speed" meters per second
         transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime); // "speed" meters per second
 
-        if(transform.position.y > 0)
+        if (transform.position.y > 0)
         {
             transform.position = new Vector3(transform.position.x, 0, 0);
-        } else if(transform.position.y < -4.2f)
+        }
+        else if (transform.position.y < -4.2f)
         {
             transform.position = new Vector3(transform.position.x, -4.2f, 0);
         }
@@ -46,7 +54,6 @@ public class Player : MonoBehaviour {
         {
             transform.position = new Vector3(9.5f, transform.position.y, 0);
         }
-
     }
 
     // --> stopped at 020
